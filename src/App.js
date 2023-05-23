41c77e0733842251ecabcb529d22684c810a8344
myusername <rabbaidibibata@yahoo.fr>
2023-05-23 11:10:40 -0400

    Created an account on chat Engine, created a user, entered users' data and created a new chat room but currently just one user to begin with

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
 import { ChatEngine } from "react-chat-engine";
 
 import ' ./App.css';
+
+const App = () => {
+    return (
+        <ChatEngine 
+        height = "100vh"
+        projectID= "f1d5881b-6d02-443f-bb02-bc74cfe2e043"
+        userName="javascriptmastery"
+        userSecret= "111111"
+
+        />
+    )
+}
+export default App;
