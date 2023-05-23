f7a92989a4b86e65cea4d9211f03f8ef838099f9
myusername <rabbaidibibata@yahoo.fr>
2023-05-23 14:58:29 -0400

    Adding news components and a new user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { ChatEngine } from "react-chat-engine";
 
-import ' ./App.css';
+import ChatFeed from './components/ChatFeed';
+
+import './App.css';
 
 const App = () => {
     return (
         <ChatEngine 
         height = "100vh"
         projectID= "f1d5881b-6d02-443f-bb02-bc74cfe2e043"
-        userName="javascriptmastery"
+        userName= "javascriptmastery"
         userSecret= "111111"
+        renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps} /> }
 
         />
     )
